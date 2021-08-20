dd71cf38da3a1d4f0590497ea5c07abc9d1613d6
vivek <vivek.iitd93@gmail.com>
2021-08-20 22:49:10 +0530

    To insert salary using prepared statement

--- Employee_DB/src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ Employee_DB/src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.List;
 
 public class EmployeePayrollServiceTest {
@@ -26,15 +25,26 @@ public class EmployeePayrollServiceTest {
 
     /**
      * Purpose : To test whether the salary is updated in the database and is synced with the DB
-     * - Read the values from the database
-     * - Update the salary in the database
-     * - Test whether the database is correctly synced or not
+     *           - Read the values from the database
+     *           - Update the salary in the database
+     *           - Test whether the database is correctly synced or not
      */
     @Test
     public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
         List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
-        employeePayrollService.updateEmployeeSalary("Vivek", 1800000);
-        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Vivek");
+        employeePayrollService.updateEmployeeSalary("vivek", 1800000);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("vivek");
+        Assert.assertTrue(result);
+    }
+
+    /**
+     * Purpose : To test whether the salary is updated in the database and is synced with the DB using JDBC PreparedStatement
+     */
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDBUsingPreparedStatement() throws EmployeePayrollException {
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalaryUsingPreparedStatement("Mukesh", 5000);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mukesh");
         Assert.assertTrue(result);
     }
 }
