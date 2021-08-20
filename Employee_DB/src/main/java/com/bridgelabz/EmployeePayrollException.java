f1b0790b8b321fde627a8563ec69ba134032b836
vivek <vivek.iitd93@gmail.com>
2021-08-20 22:33:22 +0530

    To update and sync

---
+++ Employee_DB/src/main/java/com/bridgelabz/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayrollException extends Exception {
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
--- Employee_DB/src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ Employee_DB/src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -1,11 +1,19 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
 public class EmployeePayrollServiceTest {
+    EmployeePayrollService employeePayrollService = null;
+
+    @Before
+    public void setUp() throws Exception {
+        employeePayrollService = new EmployeePayrollService();
+    }
+
     /**
      * Purpose : To test whether the number of records present in the database matches with the expected value
      */
@@ -14,5 +22,19 @@ public class EmployeePayrollServiceTest {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
         Assert.assertEquals(4, employeePayrollData.size());
+    }
+
+    /**
+     * Purpose : To test whether the salary is updated in the database and is synced with the DB
+     * - Read the values from the database
+     * - Update the salary in the database
+     * - Test whether the database is correctly synced or not
+     */
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalary("Vivek", 1800000);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Vivek");
+        Assert.assertTrue(result);
     }
 }
