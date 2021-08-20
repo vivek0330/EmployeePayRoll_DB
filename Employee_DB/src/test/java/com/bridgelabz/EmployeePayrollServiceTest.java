39da2d3c43f4314e71e2b428da9c1a802123e65f
vivek <vivek.iitd93@gmail.com>
2021-08-20 22:27:33 +0530

    to retrive data from payroll service db

---
+++ Employee_DB/src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EmployeePayrollServiceTest {
+    /**
+     * Purpose : To test whether the number of records present in the database matches with the expected value
+     */
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assert.assertEquals(4, employeePayrollData.size());
+    }
+}
